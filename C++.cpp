9bf57af103af914e93791098e2fea77d4a266cf5
Qianying-Chuugu <2270538467@qq.com>
2026-04-05 12:46:09 +0800

    4.5

--- C++.cpp
+++ C++.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 class tree{
     public:
-    tree():a(10),b(88){
-        cout<<a<<' '<<b<<endl;
-    }
-    public:
     int a;
     int b;
+    int c;
+    static double num;
+    public:
+    static void test1 (tree &exm){
+        num=99;
+        exm.a=1;
+        cout<<"yes"<<endl;
+    }
 };
+
+double tree::num = 100;
 
 int main(){
     tree branch;
-
+    tree::test1(branch);
+    cout<<branch.a<<" "<<tree::num<<endl;
 }
